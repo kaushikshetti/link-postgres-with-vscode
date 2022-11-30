bb3b9a31a9c721b62dc0ddb8f4cfdc1f489ce56b
Kaushik Shetti <kshetti29@gmail.com>
2022-11-30 13:34:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,35 +16,16 @@ const client = new Client({
 });
 
 client.connect((err) => {
-  if (err) return console.error("error: " + err.message);
-
-  client.query("DROP TABLE csvdb", (err, drop) => {
-    // Query to create table "sample"
-    var createStatament ="CREATE TABLE csvdb(id int, " + "name char(50), description char(80))";
-
-    // Creating table "sample"
+    if (err) return console.error("error: " + err.message);
+      var createStatament ="CREATE TABLE csvdb(id int, " + "name char(50), description char(80))";
     console.log("Table created successfuly")
     client.query(createStatament, (err, drop) => {
       if (err) console.log("ERROR: ", err);
+     var copy="COPY csvdb FROM 'C:/Users/kshet/OneDrive/Desktop/kloopify-api/bezkoder.csv' DELIMITER ',' CSV HEADER";
+     client.query(copy);
+    })
+})
 
-  csvtojson()
-  .fromFile("./bezkoder.csv")
-  .then ((source) => {
-    console.log("Source",source);
-    for (var i = 0; i < source.length; i++) {
-      var id = source[i]["id"],
-          name = source[i]["name"],
-          description = source[i]["description"];
-          console.log(id,name,description);
-
-    const insertSQL=`INSERT INTO csvdb (id,name,description) VALUES ('${id}','${name}','${description}')`;
-         client.query(insertSQL); 
-    }    
-    console.log("All items stored into database successfully");
-    });
-   });
-  });
-});
 
 
 
